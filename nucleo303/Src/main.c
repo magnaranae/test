afb256edc24ee6fb39ec6b7a4ec805f4e7a10c9f
guenther stangassinger <guenther.stangassinger@gmx.de>
2020-05-27 20:05:43 +0000

    adding  SWO_PrintString

--- nucleo303/Src/main.c
+++ nucleo303/Src/main.c
@@ -57,6 +57,22 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void SWO_Init(uint32_t portBits, uint32_t cpuCoreFreqHz) {
+  uint32_t SWOSpeed = 64000; /* default 64k baud rate */
+  uint32_t SWOPrescaler = (cpuCoreFreqHz / SWOSpeed) - 1; /* SWOSpeed in Hz, note that cpuCoreFreqHz is expected to be match the CPU core clock */
+
+  CoreDebug->DEMCR = CoreDebug_DEMCR_TRCENA_Msk; /* enable trace in core debug */
+  *((volatile unsigned *)(ITM_BASE + 0x400F0)) = 0x00000002; /* "Selected PIN Protocol Register": Select which protocol to use for trace output (2: SWO NRZ, 1: SWO Manchester encoding) */
+  *((volatile unsigned *)(ITM_BASE + 0x40010)) = SWOPrescaler; /* "Async Clock Prescaler Register". Scale the baud rate of the asynchronous output */
+  *((volatile unsigned *)(ITM_BASE + 0x00FB0)) = 0xC5ACCE55; /* ITM Lock Access Register, C5ACCE55 enables more write access to Control Register 0xE00 :: 0xFFC */
+  ITM->TCR = ITM_TCR_TraceBusID_Msk | ITM_TCR_SWOENA_Msk | ITM_TCR_SYNCENA_Msk | ITM_TCR_ITMENA_Msk; /* ITM Trace Control Register */
+  ITM->TPR = ITM_TPR_PRIVMASK_Msk; /* ITM Trace Privilege Register */
+  ITM->TER = portBits; /* ITM Trace Enable Register. Enabled tracing on stimulus ports. One bit per stimulus port. */
+  *((volatile unsigned *)(ITM_BASE + 0x01000)) = 0x400003FE; /* DWT_CTRL */
+  *((volatile unsigned *)(ITM_BASE + 0x40304)) = 0x00000100; /* Formatter and Flush Control Register */
+}
+
+
 int _write(int file, char *ptr, int len)
 {
 	int DataIdx;
@@ -67,6 +83,46 @@ int _write(int file, char *ptr, int len)
 	}
 
 	return len;
+}
+
+
+/*!
+ * \brief Sends a character over the SWO channel
+ * \param c Character to be sent
+ * \param portNo SWO channel number, value in the range of 0 to 31
+ */
+void SWO_PrintChar(char c, uint8_t portNo) {
+  volatile int timeout;
+
+  /* Check if Trace Control Register (ITM->TCR at 0xE0000E80) is set */
+  if ((ITM->TCR&ITM_TCR_ITMENA_Msk) == 0) { /* check Trace Control Register if ITM trace is enabled*/
+    return; /* not enabled? */
+  }
+  /* Check if the requested channel stimulus port (ITM->TER at 0xE0000E00) is enabled */
+  if ((ITM->TER & (1ul<<portNo))==0) { /* check Trace Enable Register if requested port is enabled */
+    return; /* requested port not enabled? */
+  }
+  timeout = 5000; /* arbitrary timeout value */
+  while (ITM->PORT[0].u32 == 0) {
+    /* Wait until STIMx is ready, then send data */
+    timeout--;
+    if (timeout==0) {
+      return; /* not able to send */
+    }
+  }
+  ITM->PORT[0].u16 = 0x08 | (c<<8);
+}
+
+
+/*!
+ * \brief Sends a string over SWO to the host
+ * \param s String to send
+ * \param portNumber Port number, 0-31, use 0 for normal debug strings
+ */
+void SWO_PrintString(const char *s, uint8_t portNumber) {
+  while (*s!='\0') {
+    SWO_PrintChar(*s++, portNumber);
+  }
 }
 
 /* USER CODE END 0 */
@@ -101,6 +157,11 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
+#define CPU_CORE_FREQUENCY_HZ 120000000 /* CPU core frequency in Hz */
+
+SWO_Init(0x1, CPU_CORE_FREQUENCY_HZ);
+
+
   int a = 0;
   /* USER CODE END 2 */
 
@@ -111,7 +172,8 @@ int main(void)
 	  a = a + 1;
 	  //HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
 	  HAL_Delay(1000);
-	  printf("fwef\n");
+	  //printf("fwef\n");
+	  SWO_PrintString("hello world with SWO\r\n", 0);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
